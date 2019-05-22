c82d3e114a8aa50115dc0e47fdeb3b77ebdc9481
Neci89 <berisavac89@gmail.com>
2019-05-22 15:30:03 +0200

    third commit

--- app.js
+++ app.js
@@ -62,50 +62,108 @@
 //   { name: "Seka", age: 12, job: "bla" }
 // ];
 
-
-
-
 // const adult =  array.filter( el => {
 //   return el.age > 18
 // })
 
 // console.log(adult)
 
-const generateColor = () => {
+// const generateColor = () => {
 
-  const red = Math.floor(Math.random() * 255);
-  const green = Math.floor(Math.random() * 255);
-  const blue = Math.floor(Math.random() * 255);
-  const a = Math.random();
+//   const red = Math.floor(Math.random() * 255);
+//   const green = Math.floor(Math.random() * 255);
+//   const blue = Math.floor(Math.random() * 255);
+//   const a = Math.random();
 
-  return  `rgba(${red},${green},${blue},${a})`;
- 
+//   return  `rgba(${red},${green},${blue},${a})`;
+
+// }
+
+// const changeStyle = (element, property, value) => {
+//     element.style[property] = value;
+// }
+
+// const elements = {
+//   square: document.querySelector(".main"),
+//   heading: document.querySelector(".heading"),
+//   para: document.querySelector(".para")
+// }
+
+// const changeMultipleElements = () => {
+//     changeStyle(elements.square, 'backgroundColor', generateColor()),
+//     changeStyle(elements.heading, 'color', generateColor())
+// }
+
+//  document.querySelector(".btn").addEventListener('click', changeMultipleElements);
+
+// const input = document.querySelector(".field");
+// const button = document.querySelector(".btn");
+// const slct = document.querySelector(".slct");
+
+// const write = (input, select) => {
+//   const list = document.querySelector(".list");
+//   const markup = `<p>${input}, ${select}</p>`;
+//   list.insertAdjacentHTML("beforeend", markup);
+// };
+
+// button.addEventListener("click", () => {
+//   if (input.value !== "") {
+//     write(input.value, slct.options[slct.selectedIndex].text);
+//   }else {
+//     write("Morate da unesete tekst u input", slct.options[slct.selectedIndex].text)
+//   }
+// });
+
+const write = el => {
+  const list = document.querySelector(".list");
+  const markup = `<li id=${el.id}>${el.title}</li>`;
+  list.insertAdjacentHTML("beforeend", markup);
+};
+
+const bla = param => {
+  param.map(el => {
+    write(el);
+  });
+};
+
+const renderSinglePost = post => {
+  console.log(post.body)
 }
 
-const changeStyle = (element, property, value) => {
-    element.style[property] = value;
+
+
+const getSinglePost = async (id) => {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  try {
+    const data = await response.json();
+    renderSinglePost(data)
+  }catch(err){
+    console.log(err)
+  }
+
 }
 
-const elements = {
-  square: document.querySelector(".main"),
-  heading: document.querySelector(".heading"),
-  para: document.querySelector(".para")
-}
 
-const changeMultipleElements = () => {
-    changeStyle(elements.square, 'backgroundColor', generateColor()),
-    changeStyle(elements.heading, 'color', generateColor())
-}
 
- 
+const objId = document.querySelector("ul");
 
- document.querySelector(".btn").addEventListener('click', changeMultipleElements);
+objId.addEventListener("click", () => {
+  if (event.target.tagName === "LI") {
+    getSinglePost(event.target.id)
+  }
+});
 
 
 
+const api = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
 
+  try {
+    const data = await response.json();
+    return bla(data);
+  } catch (err) {
+    // console.log(err)
+  }
+};
 
-
-
-
-
+api();
